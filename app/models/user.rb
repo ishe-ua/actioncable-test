4e1732fef96d410c19ff9d7e1be44c3a12c05b56
ishe_ua <in.shevkun@gmail.com>
2017-12-23 14:43:32 +0200

    User model

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_name  (name) UNIQUE
+#
+
+class User < ApplicationRecord
+  validates :name, unique: true
+end
---
+++ db/migrate/20171223123809_create_users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.timestamps
+    end
+
+    add_index :users, :name, unique: true
+  end
+end
