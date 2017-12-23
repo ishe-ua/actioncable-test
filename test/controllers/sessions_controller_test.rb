4296ad11cf1cf843b21028dc9882512d4e2184ee
ishe_ua <in.shevkun@gmail.com>
2017-12-23 12:46:28 +0200

    SessionsController template

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test 'should get new' do
+    skip
+  end
+
+  test 'should get create' do
+    skip
+  end
+end
